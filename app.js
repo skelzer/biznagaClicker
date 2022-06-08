c6449deca0e39ea07251bdee0f929f1cdbb6e5fb
Miguel Luque <miguel.luque@dacadoo.com>
2022-06-08 23:14:33 +0200

    added button, style file

--- app.js
+++ app.js
@@ -5,9 +5,23 @@ const http = require("http").Server(app)
 const io = require("socket.io")(http)
 const server = http.listen(port)
 
+const playerStatus = {cookies:0, primillos:0}
+
 
 app.use(express.static(__dirname+"/1/"))
 
 app.get('/', function (req, res) {
     res.sendFile('index.html');
+})
+
+function hiringFunction(kind){
+
+}
+
+io.on("connection", function (s){
+    s.on("clickMain", function (){
+        playerStatus.cookies++
+        s.emit("refresh", playerStatus)
+    })
+    s.on("hire", hiringFunction)
 })
